206548fa2880dab4db057450e658bd4269203e3c
ChristopherALee <christopheradamlee@gmail.com>
2018-04-20 18:25:56 -0400

    Moved initialization of buttons in oracle.js

--- src/main.js
+++ src/main.js
@@ -2,56 +2,9 @@ import Oracle from "./scripts/oracle.js";
 
 window.initMap = () => {
   // Styles a map in night mode.
-  const initialize = new Oracle();
-  var map = initialize.map;
+  const app = new Oracle();
+  var map = app.map;
 
   // Initializing buttons
-  const trainButton = document.getElementsByClassName("train")[0];
-  const airplaneButton = document.getElementsByClassName("airplane")[0];
-  const boatButton = document.getElementsByClassName("boat")[0];
-
-  trainButton.addEventListener("click", () => {
-    initialize.changeMode("train");
-    initialize.map.setCenter(40.706648, -73.887235);
-    initialize.map.setZoom(12);
-
-    airplaneButton.classList.remove("airplane-active");
-    airplaneButton.classList.add("airplane");
-
-    boatButton.classList.remove("boat-active");
-    boatButton.classList.add("boat");
-
-    trainButton.classList.remove("train");
-    trainButton.classList.add("train-active");
-  });
-
-  airplaneButton.addEventListener("click", () => {
-    initialize.changeMode("airplane");
-    initialize.map.setCenter(34.403065, -92.181998);
-    initialize.map.setZoom(4);
-
-    trainButton.classList.remove("train-active");
-    trainButton.classList.add("train");
-
-    boatButton.classList.remove("boat-active");
-    boatButton.classList.add("boat");
-
-    airplaneButton.classList.remove("airplane");
-    airplaneButton.classList.add("airplane-active");
-  });
-
-  boatButton.addEventListener("click", () => {
-    initialize.changeMode("boat");
-    initialize.map.setCenter(34.403065, -92.181998);
-    initialize.map.setZoom(4);
-
-    trainButton.classList.remove("train-active");
-    trainButton.classList.add("train");
-
-    airplaneButton.classList.remove("airplane-active");
-    airplaneButton.classList.add("airplane");
-
-    boatButton.classList.remove("boat");
-    boatButton.classList.add("boat-active");
-  });
+  app.initialize();
 };
--- src/scripts/oracle.js
+++ src/scripts/oracle.js
@@ -4,6 +4,56 @@ class Oracle {
   constructor() {
     this.map = new Map();
     this.mode = "none";
+    this.trainButton = document.getElementsByClassName("train")[0];
+    this.airplaneButton = document.getElementsByClassName("airplane")[0];
+    this.boatButton = document.getElementsByClassName("boat")[0];
+  }
+
+  initialize() {
+    this.trainButton.addEventListener("click", () => {
+      this.changeMode("train");
+      this.map.setCenter(40.706648, -73.887235);
+      this.map.setZoom(12);
+
+      this.airplaneButton.classList.remove("airplane-active");
+      this.airplaneButton.classList.add("airplane");
+
+      this.boatButton.classList.remove("boat-active");
+      this.boatButton.classList.add("boat");
+
+      this.trainButton.classList.remove("train");
+      this.trainButton.classList.add("train-active");
+    });
+
+    this.airplaneButton.addEventListener("click", () => {
+      this.changeMode("airplane");
+      this.map.setCenter(34.403065, -92.181998);
+      this.map.setZoom(4);
+
+      this.trainButton.classList.remove("train-active");
+      this.trainButton.classList.add("train");
+
+      this.boatButton.classList.remove("boat-active");
+      this.boatButton.classList.add("boat");
+
+      this.airplaneButton.classList.remove("airplane");
+      this.airplaneButton.classList.add("airplane-active");
+    });
+
+    this.boatButton.addEventListener("click", () => {
+      this.changeMode("boat");
+      this.map.setCenter(34.403065, -92.181998);
+      this.map.setZoom(4);
+
+      this.trainButton.classList.remove("train-active");
+      this.trainButton.classList.add("train");
+
+      this.airplaneButton.classList.remove("airplane-active");
+      this.airplaneButton.classList.add("airplane");
+
+      this.boatButton.classList.remove("boat");
+      this.boatButton.classList.add("boat-active");
+    });
   }
 
   changeMode(mode) {
